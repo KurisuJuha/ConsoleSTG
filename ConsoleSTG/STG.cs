37614dd047e5282b8ba68d084afbe6935c61d031
KYapp <kukiyomenaisapp@gmail.com>
2022-08-14 14:25:31 +0900

    弾をだす処理

--- ConsoleSTG/STG.cs
+++ ConsoleSTG/STG.cs
@@ -9,6 +9,11 @@ namespace ConsoleSTG
         public string PlayerSprite = " ^\n< > ";
         public (int x, int y) PlayerPosition = (0, 0);
 
+        public string playerBulletSprite = "^";
+        public List<(int x, int y)> playerBullets = new List<(int x, int y)>();
+
+        public bool onSpace;
+
         public void Game()
         {
             if (Keyboard.IsKeyPressed(Key.LeftArrow))
@@ -29,8 +34,32 @@ namespace ConsoleSTG
             {
                 PlayerPosition.y++;
             }
+            if (Keyboard.IsKeyPressed(Key.Space))
+            {
+                if (!onSpace)
+                {
+                    playerBullets.Add((PlayerPosition.x + 1, PlayerPosition.y));
+                }
+
+                onSpace = true;
+            }
+            else
+            {
+                onSpace = false;
+            }
+
+            foreach (var item in playerBullets)
+            {
+                JConsole.Write(item.x, item.y, playerBulletSprite);
+            }
 
             JConsole.Write(PlayerPosition.x, PlayerPosition.y, PlayerSprite);
+
+            // 弾の位置を動かす
+            for (int i = 0; i < playerBullets.Count; i++)
+            {
+                playerBullets[i] = (playerBullets[i].x, playerBullets[i].y - 1);
+            }
         }
     }
 }
